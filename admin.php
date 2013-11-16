5e77692f99f6f8326d8a0002be6d838a5430dc17
Moudondi Godfried <godfried.moudondi@te-laval.fr>
2013-11-16 14:58:25 +0100

    Google analytics snippet

--- admin.php
+++ admin.php
@@ -134,11 +134,6 @@
         <script src="js/_users.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+        <?php require_once('_ajax/gAnalytics.php'); ?>
     </body>
 </html>
--- user_edit.php
+++ user_edit.php
@@ -192,11 +192,6 @@
         <script src="js/_admin.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+        <?php require_once('_ajax/gAnalytics.php'); ?>
     </body>
 </html>
